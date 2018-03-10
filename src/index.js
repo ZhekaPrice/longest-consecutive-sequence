ec3fe6a8d0b63d1779b4decf13bb7c950f57bf8f
ZhekaPrice <36643027+ZhekaPrice@users.noreply.github.com>
2018-03-10 18:51:32 +0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function longestConsecutiveLength(array) {
-  // your solution here
+  if(array.length == 0)
+    return 0;
+  if(array.length == 1)
+    return 1;
+
+  let CurrentIndex = 0;
+  let MaxLength = 0, TempMaxLength = 0;
+  for(let i = 0; i < array.length; i++)
+  {
+    TempMaxLength++;
+    CurrentIndex = array.indexOf(array[i] + 1);
+    while(CurrentIndex != -1)
+    {
+        TempMaxLength++;
+        if(TempMaxLength > MaxLength)
+            MaxLength = TempMaxLength;
+        CurrentIndex = array.indexOf(array[CurrentIndex] + 1);
+    };
+    TempMaxLength = 0;
+  };
+  return MaxLength;
 }
